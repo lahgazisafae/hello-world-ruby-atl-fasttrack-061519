636aa9379aee74dbde5dc2ab37f694341d13268b
Safae Lahgazi Alaoui <safae.alaoui@flatironschool.com>
2019-06-15 15:26:53 +0000

    Automatically backed up by Learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,34 +1,34 @@
-# puts "Hello World"
+# # puts "Hello World"
 
-# name = "Safae"
+# # name = "Safae"
+
+# # puts name 
+
+# # number = 6 
+
+# # puts number + 3 
+
+# # pink = "pink"
+
+# # puts pink
+
+# # light_on = false
+# # puts  light_on
+# # light_on = "true"
+# # puts light_on
+
+# #puts this  is  a comment
+
+# array = [1,2,3]
+
+# login = ["safae", "123456"]
+
+# profile = ["safae","123456", 4135473333, true]
+# name = "gary"
 
 # puts name 
-
-# number = 6 
-
-# puts number + 3 
-
-# pink = "pink"
-
-# puts pink
-
-# light_on = false
-# puts  light_on
-# light_on = "true"
-# puts light_on
-
-#puts this  is  a comment
-
-array = [1,2,3]
-
-login = ["safae", "123456"]
-
-profile = ["safae","123456", 4135473333, true]
-name = "gary"
-
-puts name 
-puts profile[2]
-puts profile[0]
+# puts profile[2]
+# puts profile[0]
 
 
 
